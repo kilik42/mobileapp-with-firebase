0e3b24f1aa46f73953e3cef71b08db15028887f0
marvin evins <marvinevins@yahoo.com>
2023-04-13 10:21:22 -0500

    working on mobile interface

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const inputField = document.querySelector('input-field');
+const addBtn = document.querySelector('add-button');
+
+addBtn.addEventListener('click', () => {
+    console.log(inputField.value);
+});
