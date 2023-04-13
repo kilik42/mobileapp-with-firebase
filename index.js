44dda74669e0472f420f16c797eb07bb7a3bb900
marvin evins <marvinevins@yahoo.com>
2023-04-13 10:28:23 -0500

    working on interface

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const inputField = document.querySelector('input-field');
 const addBtn = document.querySelector('add-button');
 
 addBtn.addEventListener('click', () => {
-    console.log(inputField.value);
+    let inputValue = inputField.value;
+    console.log(inputValue);
+
+    
 });
